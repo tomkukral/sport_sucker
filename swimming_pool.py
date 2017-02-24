6fdf73de508c779752a743d36019c3c9b68f5d43
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-02-24 21:30:40 +0100

    fix formating

--- swimming_pool.py
+++ swimming_pool.py
@@ -4,7 +4,7 @@ from scrapers import PoolScraper
 
 webs = {
     'Sutka': {'url': 'http://www.sutka.eu', 'reg': r'Aktuální počet návštěvníků: (?P<people>[\d]*)'}
-    }
+}
 
 for k, v in webs.items():
     a = PoolScraper(v['url'], v['reg'])
